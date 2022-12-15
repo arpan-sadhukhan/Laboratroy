e08b1b3ed31619b463cfb0d86271df979fc619f0
Arpan Sadhukhan <92740136+arpan-sadhukhan@users.noreply.github.com>
2022-12-15 12:40:51 +0530

    Update MultiplicationTable.java

--- Assignments 15-12-22/MultiplicationTable.java
+++ Assignments 15-12-22/MultiplicationTable.java
@@ -1,16 +1,22 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
 
 	public static void main(String[] args) {
 
-		for(int i=1;i<=9;i++) {
-			for(int j=1;j<=9;j++) {
-			
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number: ");
+	
+		
+		int i=sc.nextInt();
+		
+		for(int j=1;j<=9;j++)
+		{
 			System.out.println(i+"*"+j+"="+i*j);
 		}
-			System.out.println();
-		}
+		sc.close();
 	}
 
 }
